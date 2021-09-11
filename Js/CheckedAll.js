6ce9b92b861b46454afce865c9df56b0c70b0861
Ferdino Subastian <ferdinosubastian35223@gmail.com>
2021-09-11 16:16:33 +0700

    push

--- Js/CheckedAll.js
+++ Js/CheckedAll.js
@@ -3,92 +3,107 @@ const URL = document.getElementById('url').value;
 const TOKEN = document.getElementById('token').value;
 
 checkAll.onchange = function () {
-    let cl = document.getElementsByClassName('cl');
-    if (cl !== null) {
-        if (this.checked) {
-            for (let i = 0; i < cl.length; i++) {
-                cl[i].checked = true;
-            }
-        } else {
-            for (let i = 0; i < cl.length; i++) {
-                cl[i].checked = false;
-            }
-        }
+  let cl = document.getElementsByClassName('cl');
+  if (cl !== null) {
+    if (this.checked) {
+      for (let i = 0; i < cl.length; i++) {
+        cl[i].checked = true;
+        document.getElementById('bayar').disabled = false;
+      }
+    } else {
+      for (let i = 0; i < cl.length; i++) {
+        cl[i].checked = false;
+        document.getElementById('bayar').disabled = true;
+      }
     }
+  }
 };
 
+let array = [];
+
 function ch() {
-    const cl = document.getElementsByClassName('cl')
-    for (let i = 0; i < cl.length; i++) {
-        if (cl[i].checked === true) {
-            document.getElementById('bayar').disabled = false;
-        } else {
-            document.getElementById('bayar').disabled = true;
-        }
-    }
+  const cl = document.getElementsByClassName('cl');
+  for (let i = 0; i < cl.length; i++) {
+    cl[i].onchange = function () {
+      if (cl[i].checked) {
+        document.getElementById('bayar').disabled = false;
+        array.push({ in: cl[i].value });
+      } else {
+        array.map((x) => {
+          if (x.in === cl[i].value) {
+            deleteArray(cl[i].value);
+            if (array.length === 0) {
+              document.getElementById('bayar').disabled = true;
+            }
+          }
+        });
+      }
+    };
+  }
 }
 
-ch()
+function deleteArray(val) {
+  array = array.filter((u) => u.in != val);
+}
+
+ch();
 
 document.getElementById('filter').addEventListener('click', function () {
-    getSopir();
+  getSopir();
 });
 
 document.querySelector('select[name=pilih]').onchange = function () {
-    if (this.value === 'Pilih Petani') {
-        const ds = document.querySelectorAll('input[type=checkbox]');
-        for (let i = 0; i < ds.length; i++) {
-            ds[i].disabled = true;
-        }
+  if (this.value === 'Pilih Petani') {
+    const ds = document.querySelectorAll('input[type=checkbox]');
+    for (let i = 0; i < ds.length; i++) {
+      ds[i].disabled = true;
     }
+  }
 };
 
 function getSopir() {
-    const sopir = document.querySelector('select[name=pilih]').value;
-    fetch(URL + '/pilih?name=' + sopir)
-        .then(res => res.json())
-        .then(res => {
-            document.getElementById('list-data').innerHTML = parse(
-                res.pembayaran
-            );
-            res.type === 'base' ? dCheckbox() : oCheckbox();
-            // res.type === 'base'
-            //     ? (document.getElementById('bayar').disabled = true)
-            //     : (document.getElementById('bayar').disabled = false);
-            ch();
-        });
+  const sopir = document.querySelector('select[name=pilih]').value;
+  fetch(URL + '/pilih?name=' + sopir)
+    .then((res) => res.json())
+    .then((res) => {
+      document.getElementById('list-data').innerHTML = parse(res.pembayaran);
+      res.type === 'base' ? dCheckbox() : oCheckbox();
+      // res.type === 'base'
+      //     ? (document.getElementById('bayar').disabled = true)
+      //     : (document.getElementById('bayar').disabled = false);
+      ch();
+    });
 }
 
-const parse = data => {
-    let html = '';
-    data.map(res => {
-        html += htmldata(res);
-    });
-    return html;
+const parse = (data) => {
+  let html = '';
+  data.map((res) => {
+    html += htmldata(res);
+  });
+  return html;
 };
 
 const dCheckbox = () => {
-    const ds = document.querySelectorAll('input[type=checkbox]');
-    for (let i = 0; i < ds.length; i++) {
-        ds[i].disabled = true;
-    }
+  const ds = document.querySelectorAll('input[type=checkbox]');
+  for (let i = 0; i < ds.length; i++) {
+    ds[i].disabled = true;
+  }
 };
 
 const oCheckbox = () => {
-    const ds = document.querySelectorAll('input[type=checkbox]');
-    for (let i = 0; i < ds.length; i++) {
-        ds[i].disabled = false;
-    }
+  const ds = document.querySelectorAll('input[type=checkbox]');
+  for (let i = 0; i < ds.length; i++) {
+    ds[i].disabled = false;
+  }
 };
 
-const htmldata = item => {
-    const total = item.harga * item.netto_pulang;
-    if (item.tanggal_bayar != null) {
-        return /* html */ `
-        <td colspan="9" style="text-align: center;">DATA KOSONG</td>`
-    }
-    else {
-        return /*html*/ `<tr>
+const htmldata = (item) => {
+  const total = item.harga * item.netto_pulang;
+  if (item.tanggal_bayar != null) {
+    return /* html */ `
+        <td colspan="9" style="text-align: center;">DATA KOSONG</td>`;
+  } else {
+    return /*html*/ `<tr>
     <td><input type="checkbox" class="cl" name="id[]" value="${item.id_keberangkatan}" disabled /></td>
     <td>${formatTanggal(item.tanggal_pulang)}</td>
     <td>${item.tipe}</td>
@@ -103,41 +118,28 @@ const htmldata = item => {
     <td>${formatRupiah(item.harga.toString(), 'Rp ')}</td>
     <td>${formatRupiah(total.toString(), 'Rp ')}</td>
 </tr>`;
-    }
-
+  }
 };
 
-const formatTanggal = tgl => {
-    const listMonth = [
-        'Januari',
-        'Februari',
-        'Maret',
-        'April',
-        'Mei',
-        'Juni',
-        'Juli',
-        'Agustus',
-        'September',
-        'November',
-        'Desember',
-    ];
-    const month = tgl.split('-');
-    return `${month[2]}/${month[1]}/${month[0]}`;
+const formatTanggal = (tgl) => {
+  const listMonth = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'November', 'Desember'];
+  const month = tgl.split('-');
+  return `${month[2]}/${month[1]}/${month[0]}`;
 };
 
 const formatRupiah = (angka, prefix) => {
-    var number_string = angka.replace(/[^,\d]/g, '').toString(),
-        split = number_string.split(','),
-        sisa = split[0].length % 3,
-        rupiah = split[0].substr(0, sisa),
-        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+  var number_string = angka.replace(/[^,\d]/g, '').toString(),
+    split = number_string.split(','),
+    sisa = split[0].length % 3,
+    rupiah = split[0].substr(0, sisa),
+    ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
-    // tambahkan titik jika yang di input sudah menjadi angka ribuan
-    if (ribuan) {
-        separator = sisa ? '.' : '';
-        rupiah += separator + ribuan.join('.');
-    }
+  // tambahkan titik jika yang di input sudah menjadi angka ribuan
+  if (ribuan) {
+    separator = sisa ? '.' : '';
+    rupiah += separator + ribuan.join('.');
+  }
 
-    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-    return prefix == undefined ? rupiah : rupiah ? 'Rp. ' + rupiah : '';
+  rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+  return prefix == undefined ? rupiah : rupiah ? 'Rp. ' + rupiah : '';
 };
